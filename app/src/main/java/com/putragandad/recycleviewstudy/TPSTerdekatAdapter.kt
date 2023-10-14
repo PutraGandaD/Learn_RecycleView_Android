3b1dccf6a63d85ecae576f107b01603b865da3d6
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-10-14 15:55:09 +0700

    Finished Adapter

--- app/src/main/java/com/putragandad/recycleviewstudy/TPSTerdekatAdapter.kt
+++ app/src/main/java/com/putragandad/recycleviewstudy/TPSTerdekatAdapter.kt
@@ -1,5 +1,41 @@
 package com.putragandad.recycleviewstudy
 
-class TPSTerdekatAdapter(private val TPSTerdekatList : ArrayList<TPSTerdekat>) {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.putragandad.recycleviewstudy.databinding.ActivityMainBinding
+import com.putragandad.recycleviewstudy.databinding.RecentvisitItemBinding
 
+class TPSTerdekatAdapter(private val TPSTerdekatList : ArrayList<TPSTerdekat>)
+    : RecyclerView.Adapter<TPSTerdekatAdapter.TpsViewHolder>() {
+
+    class TpsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageTPS : ImageView = itemView.findViewById(R.id.iv_tps)
+        val namaTPS : TextView = itemView.findViewById(R.id.tv_nama_tps)
+        val jarakTPS : TextView = itemView.findViewById(R.id.tv_jarak_tps)
+        val alamatTPS : TextView = itemView.findViewById(R.id.tv_alamat_tps)
+        val jenisTPS : TextView = itemView.findViewById(R.id.tv_jenis_tps)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TpsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recentvisit_item
+        , parent, false)
+        return TpsViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return TPSTerdekatList.size
+    }
+
+    override fun onBindViewHolder(holder: TpsViewHolder, position: Int) {
+        val tps = TPSTerdekatList[position]
+        holder.imageTPS.setImageResource(tps.image)
+        holder.namaTPS.text = tps.namaTPS
+        holder.jarakTPS.text = tps.jarakTPS
+        holder.alamatTPS.text = tps.alamatTPS
+        holder.jenisTPS.text = tps.jenisTPS
+    }
 }
